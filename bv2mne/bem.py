a4bbea94fde8e98d00b647b26233b53b2508a7d8
David Meunier <david_meunier_79@hotmail.fr>
2020-02-07 17:18:27 +0100

    Modified main for argparse and all other file accordingly for json.
    directories.py now contains function, and is not imported with * anymore

--- bv2mne/bem.py
+++ bv2mne/bem.py
@@ -3,54 +3,35 @@
 
 import mne
 
-from directories import *
+import os.path as op
 
-def create_bem(subject):
-    """ Create the BEM model from FreeSurfer files
+from bv2mne.directories import read_directories, read_databases
 
-    Parameters:
-    ----------
-    subject : str
-        Name of the subject to calculate the BEM model
-
-    Returns:
-    -------
-    surfaces : list of dict
-        BEM surfaces
-    bem : instance of ConductorModel
-        BEM model
-    -------
-    """
+def create_bem(json_fname, subject):
 
     print('\n---------- Resolving BEM model and BEM soultion ----------\n')
+
+
+    database, project, db_mne, db_bv, db_fs = read_databases(json_fname)
+
+    raw_dir, prep_dir, trans_dir, mri_dir, src_dir, bem_dir, fwd_dir, hga_dir = read_directories(json_fname)
 
     fname_bem_model = op.join(bem_dir.format(subject), '{0}-bem-model.fif'.format(subject))
     fname_bem_sol = op.join(bem_dir.format(subject), '{0}-bem-sol.fif'.format(subject))
 
     # Make bem model: single-shell model. Depends on anatomy only.
-    bem_model = mne.make_bem_model(subject, ico=None, conductivity=[0.3], subjects_dir=op.join(db_fs, project))
-    mne.write_bem_surfaces(fname_bem_model, bem_model)
+    model = mne.make_bem_model(subject, ico=None, conductivity=[0.3], subjects_dir=op.join(db_fs, project))
+    mne.write_bem_surfaces(fname_bem_model, model)
 
     # Make bem solution. Depends on anatomy only.
-    bem_sol = mne.make_bem_solution(bem_model)
+    bem_sol = mne.make_bem_solution(model)
     mne.write_bem_solution(fname_bem_sol, bem_sol)
 
-    return bem_model, bem_sol
+    return
 
-def check_bem(subject):
-    """ Check if the BEM model exists
+def check_bem(json_fname, subject):
 
-    Parameters
-    ----------
-    subject : str
-        The name of the subject to check the BEM model
-
-    Returns:
-    -------
-    True/False : bool
-        True if the BEM model exists for the subject, otherwise False
-    -------
-    """
+    raw_dir, prep_dir, trans_dir, mri_dir, src_dir, bem_dir, fwd_dir, hga_dir = read_directories(json_fname)
 
     # Check if BEM files exists, return boolean value
     print('\nChecking BEM files\n')
